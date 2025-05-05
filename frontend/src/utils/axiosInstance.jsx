817789bbf87753d3c46f8b9d8eafeb81a109fb62
Nishant Kumar <nishantkumar1265@gmail.com>
2025-05-05 15:30:47 +0530

    New Changes in Database Connection

--- frontend/src/utils/axiosInstance.jsx
+++ frontend/src/utils/axiosInstance.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://rideinmotihari-zgfk.onrender.com',
 });
 
 axiosInstance.interceptors.request.use((config) => {
