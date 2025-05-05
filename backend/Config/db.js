8334420012ec2d044a45ad13865e88b21ed92a9f
Nishant Kumar <nishantkumar1265@gmail.com>
2025-05-05 15:17:32 +0530

    New Changes in Database Connection

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -1,6 +1,11 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.MYDATABASE_URL);
+const sequelize = new Sequelize(process.env.MYDATABASE_URL, {
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: { rejectUnauthorized: true }  // optional (only if Railway enforces SSL)
+  }
+});
 
 module.exports = sequelize;
